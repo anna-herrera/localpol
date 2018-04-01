91e6b8d6a32a8bdf820e4d6cdaab4cfedbc48c83
Anna Herrera <aherrera8@gatech.edu>
2018-04-01 19:06:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var firebase = require("firebase");
+
+var config = {
+    apiKey: ,
+    authDomain: "localpol-c2a9b.firebaseapp.com",
+    databaseURL: "https://localpol-c2a9b.firebaseio.com",
+    projectId: "localpol-c2a9b",
+    storageBucket: "localpol-c2a9b.appspot.com",
+    messagingSenderId: "393583030469"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
