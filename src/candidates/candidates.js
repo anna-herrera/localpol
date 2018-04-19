73c33e0feaf2c901686ed276b0728d8a61c30ff3
Daniel Chung <danielchung@lawn-128-61-20-163.lawn.gatech.edu>
2018-04-19 04:50:57 -0400

    made editing candidate profile more intuitive

--- src/candidates/candidates.js
+++ src/candidates/candidates.js
@@ -45,6 +45,52 @@ router.get('/register', function(req, res) {
   res.render('candidate_pages/register');
 })
 
+router.get('/candidate/:id/view', function(req, res) {
+  // have to make sure that the id is in the users database
+  var userId = req.params['id'];
+  console.log('user id is: ' + userId);
+
+  admin.auth().getUser(userId)
+    .then(function(userRecord) {
+        adminFb.querySpecificCandidate(userId).then(function(data) {
+          if (data) { // indicates that user profile exists
+            var candidateData = data;
+            adminFb.readElectionsPromise()
+              .then(function(data) {
+                var elections = data;
+                var photo = (userRecord.photoURL) ? userRecord.photoURL : '/anna.jpg'
+                res.render('candidate_pages/viewCandidate', {
+                  electionList: elections, 
+                  userId: userId, 
+                  data: candidateData,
+                  photoURL: photo
+                });
+              })
+          } else {
+            // should create data for candidate
+            // get name from admin auth
+
+            var displayName = userRecord.displayName;
+            if (!displayName)
+                displayName = "Display name not set";
+            var candidateData = {
+              bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              name: displayName,
+              platform: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            }
+            adminFb.updateCandidate(userId, candidateData);
+            res.redirect('/candidates/candidate/' + userId + '/view');      
+          }
+        }).catch(function(err) {
+          console.log(err);
+        });
+    })
+    .catch(function(error) {
+      console.log("Error fetching user data:", error);
+      res.redirect('/candidates/login');
+    });
+});
+
 router.get('/candidate/:id/edit', function(req, res) {
   // have to make sure that the id is in the users database
   var userId = req.params['id'];
@@ -74,9 +120,9 @@ router.get('/candidate/:id/edit', function(req, res) {
             if (!displayName)
                 displayName = "Display name not set";
             var candidateData = {
-              platform: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
               name: displayName,
-              bio: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+              platform: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
             }
             adminFb.updateCandidate(userId, candidateData);
             res.redirect('/candidates/candidate/' + userId + '/edit');      
@@ -89,7 +135,7 @@ router.get('/candidate/:id/edit', function(req, res) {
       console.log("Error fetching user data:", error);
       res.redirect('/candidates/login');
     });
-
+});
 
   // adminFb.querySpecificCandidate(userId).then(function(data) {
   //   if (data) { // indicates that user exists
@@ -170,13 +216,13 @@ router.get('/candidate/:id/edit', function(req, res) {
   //             console.log(error);
   //             res.send(error);
   //     });
-});
+
 
 router.post('/candidate/:id/edit', function(req, res) {
   var userId =req.params.id;
   var newData = req.body;
   adminFb.updateCandidate(userId, newData);
-  res.redirect('/candidates/candidate/' + userId + '/edit');
+  res.redirect('/candidates/candidate/' + userId + '/view');
 });
 
 router.post('/candidate/:id/addElection', function(req, res) {
@@ -198,7 +244,7 @@ router.post('/candidate/:id/addElection', function(req, res) {
           console.log("Successfully fetched user data:", userRecord.toJSON());
           var displayName = userRecord.displayName;
           adminFb.electionAddCandidate(newElectionId, userId, displayName);
-          res.redirect('/candidates/candidate/' + userId + '/edit');
+          res.redirect('/candidates/candidate/' + userId + '/view');
         })
         .catch(function(error) {
           console.log("Error fetching user data:", error);
